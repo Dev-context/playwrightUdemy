10005eddc3b619371720d0a1150ff4f9e34f1cf4
Vinicius Oliveira Frasson <vinicius.smtp.cmd@gmail.com>
2025-02-28 23:48:42 +0000

    add Alure-report to the project

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,13 +24,15 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter:[["line"],["allure-playwright"]],
   
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASEURL,
+    screenshot:'on',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
